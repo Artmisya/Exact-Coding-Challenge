f9feb483d591f12ba0925babf02a4010b220f721
saeedeh <saeedeh@tiseno.com>
2018-06-28 21:38:21 +0800

    first version

---
+++ Contact/Networking/NetworkServiceResult.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkServiceReply.swift
+//  Contact
+//
+//  Created by Saeedeh on 27/06/2018.
+//  Copyright © 2018 tiseno. All rights reserved.
+//
+
+import Foundation
+
+enum Result{
+    case success(Data)
+    case failure(Error)
+    
+}
