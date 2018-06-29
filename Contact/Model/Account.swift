3751d45ebb77f2f67a86a127364bcc0aad760f50
saeedeh <saeedeh@tiseno.com>
2018-06-29 14:37:35 +0800

    check for empty values in decoder function

--- Contact/Model/Account.swift
+++ Contact/Model/Account.swift
@@ -8,146 +8,234 @@
 
 import Foundation
 
-
-class Account:Codable {
+class Account:Decodable {
     
     let account:String
-    var businessEmail:[Value]
-    var businessPhone:[Value]
-    var businessMobile:[Value]
-    var email:[Value]
-    var firstName:[Value]
-    var fullName:[Value]
-    var gender:[Value]
-    var id:[Value]
-    var jobTitleDescription:[Value]
-    var lastName:[Value]
-    var middleName:[Value]
-    var mobile:[Value]
-    var notes:[Value]
-    var phone:[Value]
-    var pictureThumbnailUrl:[Value]
+    var businessEmail=[Value]()
+    var businessPhone=[Value]()
+    var businessMobile=[Value]()
+    var email=[Value]()
+    var firstName=[Value]()
+    var fullName=[Value]()
+    var gender=[Value]()
+    var id=[Value]()
+    var jobTitleDescription=[Value]()
+    var lastName=[Value]()
+    var middleName=[Value]()
+    var mobile=[Value]()
+    var notes=[Value]()
+    var phone=[Value]()
+    var pictureThumbnailUrl=[Value]()
     
     private var incomplete:Bool=false
     private var mismatched:Bool=false
-
     
-    init(account:String,businessEmail:[Value],businessPhone:[Value],
-         businessMobile:[Value],email:[Value],firstName:[Value],
-         fullName:[Value],gender:[Value],id:[Value],
-         jobTitleDescription:[Value],lastName:[Value],
-         middleName:[Value],mobile:[Value],notes:[Value],
-         phone:[Value],pictureThumbnailUrl:[Value]){
+    private enum CodingKeys: String, CodingKey {
         
-        self.account=account
-        self.businessEmail=businessEmail
-        self.businessPhone=businessPhone
-        self.businessMobile=businessMobile
-        self.email=email
-        self.firstName=firstName
-        self.fullName=fullName
-        self.gender=gender
-        self.id=id
-        self.jobTitleDescription=jobTitleDescription
-        self.lastName=lastName
-        self.middleName=middleName
-        self.mobile=mobile
-        self.notes=notes
-        self.phone=phone
-        self.pictureThumbnailUrl=pictureThumbnailUrl
-        
+        case account = "Account"
+        case businessEmail = "BusinessEmail"
+        case businessPhone = "BusinessPhone"
+        case businessMobile = "BusinessMobile"
+        case email = "Email"
+        case firstName = "FirstName"
+        case fullName = "FullName"
+        case gender = "Gender"
+        case id = "ID"
+        case jobTitleDescription = "JobTitleDescription"
+        case lastName = "LastName"
+        case middleName = "MiddleName"
+        case mobile = "Mobile"
+        case notes = "Notes"
+        case phone = "Phone"
+        case pictureThumbnailUrl = "PictureThumbnailUrl"
     }
     
-    func merge(contact:Contact){
+
+    required init(from decoder: Decoder) throws {
         
-        if self.businessEmail.contains(where:{$0.data == contact.businessEmail[0].data})==false{
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        account = try container.decode(String.self, forKey: .account)
+        
+        if let businessEmailData = try container.decodeIfPresent(String.self, forKey: .businessEmail)
+              ,businessEmailData != ""  {
+             businessEmail.append(Value(data:businessEmailData))
+        }
+        if let businessPhoneData = try container.decodeIfPresent(String.self, forKey: .businessPhone)
+                ,businessPhoneData != ""  {
+             businessPhone.append(Value(data: businessPhoneData))
+        }
+        if let businessMobileData  = try container.decodeIfPresent(String.self, forKey: .businessMobile)
+                ,businessMobileData != ""  {
+                businessMobile.append(Value(data: businessMobileData))
+        }
+        if let emailData = try container.decodeIfPresent(String.self, forKey: .email)
+                ,emailData != ""  {
+             email.append(Value(data: emailData))
+        }
+        if let firstNameData = try container.decodeIfPresent(String.self, forKey: .firstName)
+                ,firstNameData != ""  {
+                firstName.append(Value(data: firstNameData))
+        }
+        if let fullNameData = try container.decodeIfPresent(String.self, forKey: .fullName)
+                ,fullNameData != ""  {
+
+                fullName.append(Value(data: fullNameData))
+        }
+        if let idData = try container.decodeIfPresent(String.self, forKey: .id)
+                ,idData != ""  {
+                id.append(Value(data: idData))
+        }
+        if let genderData = try container.decodeIfPresent(String.self, forKey: .gender)
+                ,genderData != ""  {
+                gender.append(Value(data: genderData))
+        }
+        if let lastNameData = try container.decodeIfPresent(String.self, forKey: .lastName)
+                ,lastNameData != ""  {
+                lastName.append(Value(data: lastNameData))
+        }
+        if let middleNameData = try container.decodeIfPresent(String.self, forKey: .middleName)
+                ,middleNameData != ""  {
+                middleName.append(Value(data: middleNameData))
+        }
+        if let jobTitleDescriptionData = try container.decodeIfPresent(String.self,forKey: .jobTitleDescription)
+                 ,jobTitleDescriptionData != ""  {
+                jobTitleDescription.append(Value(data: jobTitleDescriptionData))
+        }
+        if let mobileData = try container.decodeIfPresent(String.self, forKey: .mobile)
+                ,mobileData != ""  {
+                mobile.append(Value(data: mobileData))
+        }
+        if let notesData = try container.decodeIfPresent(String.self, forKey: .notes)
+                ,notesData != ""  {
+                notes.append(Value(data: notesData))
+        }
+        if let phoneData = try container.decodeIfPresent(String.self, forKey: .phone)
+                ,phoneData != ""  {
+                phone.append(Value(data: phoneData))
+        }
+        if let pictureThumbnailUrlData = try container.decodeIfPresent(String.self, forKey: .pictureThumbnailUrl)
+                ,pictureThumbnailUrlData != ""  {
+                pictureThumbnailUrl.append(Value(data: pictureThumbnailUrlData))
+        }
+    }
+
+//    init(account:String,businessEmail:[Value],businessPhone:[Value],
+//         businessMobile:[Value],email:[Value],firstName:[Value],
+//         fullName:[Value],gender:[Value],id:[Value],
+//         jobTitleDescription:[Value],lastName:[Value],
+//         middleName:[Value],mobile:[Value],notes:[Value],
+//         phone:[Value],pictureThumbnailUrl:[Value]){
+//
+//        self.account=account
+//        self.businessEmail=businessEmail
+//        self.businessPhone=businessPhone
+//        self.businessMobile=businessMobile
+//        self.email=email
+//        self.firstName=firstName
+//        self.fullName=fullName
+//        self.gender=gender
+//        self.id=id
+//        self.jobTitleDescription=jobTitleDescription
+//        self.lastName=lastName
+//        self.middleName=middleName
+//        self.mobile=mobile
+//        self.notes=notes
+//        self.phone=phone
+//        self.pictureThumbnailUrl=pictureThumbnailUrl
+//
+//    }
+    
+    func merge(account:Account){
+        
+        if account.businessEmail.count > 0 , self.businessEmail.contains(where:{$0.data == account.businessEmail[0].data})==false{
             
-            self.businessEmail.append(contact.businessEmail[0])
+            self.businessEmail.append(account.businessEmail[0])
             self.mismatched=true
             
         }
-        if self.businessPhone.contains(where:{$0.data == contact.businessPhone[0].data})==false{
+        if account.businessPhone.count > 0 , self.businessPhone.contains(where:{$0.data == account.businessPhone[0].data})==false{
             
-            self.businessPhone.append(contact.businessPhone[0])
+            self.businessPhone.append(account.businessPhone[0])
             self.mismatched=true
             
         }
-        if self.businessMobile.contains(where:{$0.data == contact.businessMobile[0].data})==false{
+        if account.businessPhone.count > 0 ,self.businessMobile.contains(where:{$0.data == account.businessMobile[0].data})==false{
             
-            self.businessMobile.append(contact.businessMobile[0])
+            self.businessMobile.append(account.businessMobile[0])
             self.mismatched=true
             
         }
-        if self.email.contains(where:{$0.data == contact.email[0].data})==false{
+        if account.email.count > 0 ,self.email.contains(where:{$0.data == account.email[0].data})==false{
             
-            self.email.append(contact.email[0])
+            self.email.append(account.email[0])
             self.mismatched=true
             
         }
-        if self.firstName.contains(where:{$0.data == contact.firstName[0].data})==false{
+        if account.firstName.count > 0 ,self.firstName.contains(where:{$0.data == account.firstName[0].data})==false{
             
-            self.firstName.append(contact.firstName[0])
+            self.firstName.append(account.firstName[0])
             self.mismatched=true
             
         }
-        if self.fullName.contains(where:{$0.data == contact.fullName[0].data})==false{
+        if account.fullName.count > 0 ,self.fullName.contains(where:{$0.data == account.fullName[0].data})==false{
             
-            self.fullName.append(contact.fullName[0])
+            self.fullName.append(account.fullName[0])
             self.mismatched=true
             
         }
-        if self.gender.contains(where:{$0.data == contact.gender[0].data})==false{
+        if account.gender.count > 0 ,self.gender.contains(where:{$0.data == account.gender[0].data})==false{
             
-            self.gender.append(contact.gender[0])
+            self.gender.append(account.gender[0])
             self.mismatched=true
             
         }
-        if self.id.contains(where:{$0.data == contact.id[0].data})==false{
+        if account.id.count > 0 ,self.id.contains(where:{$0.data == account.id[0].data})==false{
             
-            self.id.append(contact.id[0])
+            self.id.append(account.id[0])
             self.mismatched=true
             
         }
-        if self.jobTitleDescription.contains(where:{$0.data == contact.jobTitleDescription[0].data})==false{
+        if account.jobTitleDescription.count > 0 , self.jobTitleDescription.contains(where:{$0.data == account.jobTitleDescription[0].data})==false{
             
-            self.jobTitleDescription.append(contact.jobTitleDescription[0])
+            self.jobTitleDescription.append(account.jobTitleDescription[0])
             self.mismatched=true
             
         }
-        if self.lastName.contains(where:{$0.data == contact.lastName[0].data})==false{
+        if account.lastName.count > 0 ,self.lastName.contains(where:{$0.data == account.lastName[0].data})==false{
             
-            self.lastName.append(contact.lastName[0])
+            self.lastName.append(account.lastName[0])
             self.mismatched=true
             
         }
-        if self.middleName.contains(where:{$0.data == contact.middleName[0].data})==false{
+        if account.middleName.count > 0 ,self.middleName.contains(where:{$0.data == account.middleName[0].data})==false{
             
-            self.middleName.append(contact.middleName[0])
+            self.middleName.append(account.middleName[0])
             self.mismatched=true
             
         }
-        if self.mobile.contains(where:{$0.data == contact.mobile[0].data})==false{
+        if account.mobile.count > 0 ,self.mobile.contains(where:{$0.data == account.mobile[0].data})==false{
             
-            self.mobile.append(contact.mobile[0])
+            self.mobile.append(account.mobile[0])
             self.mismatched=true
             
         }
-        if self.notes.contains(where:{$0.data == contact.notes[0].data})==false{
+        if account.notes.count > 0 ,self.notes.contains(where:{$0.data == account.notes[0].data})==false{
             
-            self.notes.append(contact.notes[0])
+            self.notes.append(account.notes[0])
             self.mismatched=true
             
         }
-        if self.phone.contains(where:{$0.data == contact.phone[0].data})==false{
+        if account.phone.count > 0 ,self.phone.contains(where:{$0.data == account.phone[0].data})==false{
             
-            self.phone.append(contact.phone[0])
+            self.phone.append(account.phone[0])
             self.mismatched=true
             
         }
         
-        if self.pictureThumbnailUrl.contains(where:{$0.data == contact.pictureThumbnailUrl[0].data})==false{
+        if account.pictureThumbnailUrl.count > 0 ,self.pictureThumbnailUrl.contains(where:{$0.data == account.pictureThumbnailUrl[0].data})==false{
             
-            self.pictureThumbnailUrl.append(contact.pictureThumbnailUrl[0])
+            self.pictureThumbnailUrl.append(account.pictureThumbnailUrl[0])
             self.mismatched=true
             
         }
@@ -155,13 +243,16 @@ class Account:Codable {
     }
     
     func isMismatched()->Bool{
-    
+
         return self.mismatched
     }
-    
+
     func isIncomplete()->Bool{
-        
+
         return self.incomplete
     }
     
 }
+
+
+
