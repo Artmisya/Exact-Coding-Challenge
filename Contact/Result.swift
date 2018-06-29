d9a4022c99c8f199b30233445086015768d1cdc5
saeedeh <saeedeh@tiseno.com>
2018-06-29 17:32:45 +0800

    Version 1.o

---
+++ Contact/Result.swift
@@ -0,0 +1,15 @@
+//
+//  Result.swift
+//  Contact
+//
+//  Created by Saeedeh on 28/06/2018.
+//  Copyright © 2018 tiseno. All rights reserved.
+//
+
+import Foundation
+
+enum Result<T>{
+    case success(T)
+    case failure(Error)
+    
+}
