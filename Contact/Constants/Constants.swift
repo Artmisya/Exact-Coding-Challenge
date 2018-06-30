db6449da59f7404357cffdd7565d477b21307268
saeedeh <saeedeh@tiseno.com>
2018-06-30 23:21:22 +0800

    updated ErrorMessage and  errorDomain

--- Contact/Constants/Constants.swift
+++ Contact/Constants/Constants.swift
@@ -11,19 +11,23 @@ import Foundation
 struct Constants {
     
     struct ApiUrl{
-        static let getContactsUrl:String="https://firebasestorage.googleapis.com/v0/b/contacts-8d05b.appspot.com/o/contacts.json?alt=media&token=431c2754-b3f9-4485-8c5d-0365d5f8f0e5"
+        static let getContacts:String="https://firebasestorage.googleapis.com/v0/b/contacts-8d05b.appspot.com/o/contacts.json?alt=media&token=431c2754-b3f9-4485-8c5d-0365d5f8f0e5"
     }
 
     struct ErrorMessage{
         
         static let badUrl:String="Bad Url"
         static let serverError:String="Server Error"
+        static let apiError:String="Data received from Api is not in a correct format."
+        static let accountCompulsoryFields:String="Please make sure all compulsory fields are filled in."
         static let unknownError="Whoops, something went wrong!"
  
     }
     struct DomainError{
-        static let serverError:String="ServerError"
-        static let httpError:String="HttpError"
-        static let jsonError:String="jsonError"
+        static let server:String="ServerError"
+        static let http:String="HttpError"
+        static let json:String="jsonError"
+        static let account:String="accountError"
+        static let api:String="apiError"
     }
 }
