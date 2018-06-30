f0847ba2e883103f6086422aeceeddcc878fbbc0
saeedeh <saeedeh@tiseno.com>
2018-06-30 16:39:00 +0800

    this version read from json file

--- Contact/Model/CompanyContact.swift
+++ Contact/Model/CompanyContact.swift
@@ -16,71 +16,89 @@ final class CompanyContact{
     
     private(set) var contactsList=[Account]()
     
-    func fetchContacts(complationHandler:@escaping (Result<Any>) -> Void){
+    func fetchContacts(complationHandler:@escaping (Result<[Account]>) -> Void){
         
         guard let url=URL(string: Constants.ApiUrl.getContacts) else {
             
-            let error=NSError(domain: Constants.DomainError.httpError, code: 0, userInfo: [NSLocalizedDescriptionKey : Constants.ErrorMessage.badUrl])
+            let error=NSError(domain: Constants.DomainError.http, code: 0, userInfo: [NSLocalizedDescriptionKey : Constants.ErrorMessage.badUrl])
             
-            let result=Result<Any>.failure(error)
+            let result=Result<[Account]>.failure(error)
             complationHandler(result)
             return
         }
         
-//        NetworkService.request(url: url) { (result) in
-//
-//            switch result{
-//
-//                case .success(let data):
-//
-//                    self.processContactsData(data:data as! [String : Any])
-//                    let result=Result.success(self.contactsList as Any)
-//                    complationHandler(result)
-//
-//                case .failure(_):
-//                    complationHandler(result)
-//            }
-//        }
+        NetworkService.request(url: url) { (result) in
+
+            switch result{
+
+                case .success(let data):
+
+                    self.createContactsListFromData(data:data)
+                    let result=Result.success(self.contactsList)
+                    complationHandler(result)
+
+                case .failure(let error):
+                    let result=Result<[Account]>.failure(error)
+                    complationHandler(result)
+            }
+        }
         
         // create [Account] from Data received from API
-        let resultCreateContactList=self.createContactsListFromData()
+       /* let resultCreateContactList=self.createContactsListFromData()
         switch resultCreateContactList {
             
             case .success(let data):
                 // init the self.contactsList
-                self.contactsList=data as! [Account]
-                let result=Result.success(self.contactsList as Any)
+                self.contactsList=data 
+                let result=Result.success(self.contactsList)
                 complationHandler(result)
             
-            case .failure(_):
-                complationHandler(resultCreateContactList)
-        }
+            case .failure(let error):
+                let result=Result<[Account]>.failure(error)
+                complationHandler(result)
+        }*/
       
     }
     
-    private func createContactsListFromData()->Result<Any>{
+//    private func createContactsListFromData()->Result<[Account]>{
+//
+//            // first: decode Data into [Account]
+//            let decodeResult=self.decodeContactsData()
+//            switch decodeResult{
+//
+//                case .success(let data):
+//                    let decodedContactsList = data
+//                        // second: merge similar accounts into one account
+//                        let mergedAccountsList=self.mergeSimilarAccounts(decodedContactsList)
+//                        let result=Result.success(mergedAccountsList)
+//                        return result
+//
+//                case .failure(let error):
+//                    let result=Result<[Account]>.failure(error)
+//                    return result
+//            }
+//    }
+    private func createContactsListFromData(data:[String:Any])->Result<[Account]>{
         
-            // first: decode Data into [Account]
-            let decodeResult=self.decodeContactsData()
-            switch decodeResult{
+        if let d=data["d"]  as? [String: Any]{
+
+            if let results=d["results"] as?  [NSDictionary]{
                 
-                case .success(let data):
-                    let decodedContactsList = data as! [Account]
-                        // second: merge similar accounts into one account
-                        let mergedAccountsList=self.mergeSimilarAccounts(decodedContactsList)
-                        let result=Result.success(mergedAccountsList as Any)
-                        return result
+                let jsonDecoder = JSONDecoder()
                 
-                case .failure(_):
-                    return decodeResult
-            }
-    }
-    
-//        if let d=data["d"]  as? [String: Any]{
-//
-//            if let results=d["results"] as?  [NSDictionary]{
-//
-//                var resultsData: Data = NSKeyedArchiver.archivedData(withRootObject: results[1]) as Data
+                do {
+                    
+                    let resultsData=try JSONSerialization.data(withJSONObject: results, options: JSONSerialization.WritingOptions.prettyPrinted) as NSData
+                    let decodedContactsList=try jsonDecoder.decode([Account].self, from: resultsData as Data)
+                    
+                    print ("******",contactsList[0].notes[0].data)
+                } catch let error {
+                    
+                    let result=Result<[Account]>.failure(error)
+                    return result
+                }
+
+//                let resultsData: Data = NSKeyedArchiver.archivedData(withRootObject: results[1]) as Data
 //                let dictionary = NSKeyedUnarchiver.unarchiveObject(with: resultsData as Data) as? NSDictionary
 //
 //                let jsonDecoder = JSONDecoder()
@@ -91,16 +109,20 @@ final class CompanyContact{
 //                   // String(data: bytes, encoding: .utf8)
 //                    let jsonString=String(decoding: resultsData, as: UTF8.self)
 //                    print("************",jsonString)
-//                    print ("jjjjjjjjjjj",resultsData,dictionary!)
+//                    print ("jjjjjjjjjjj",resultsData,dictionary.data)
 //                    let contactsList=try jsonDecoder.decode(Account.self, from: resultsData)
 //                }
 //                catch let error{
 //
-//                    print (error.localizedDescription)
+//                    print (error)
 //                }
-//            }
-//        }
-  
+            }
+        }
+        
+        let result=Result.success(contactsList)
+        return result
+       
+    }
     private func mergeSimilarAccounts(_ accountsList:[Account])->[Account]{
         
         var mergedAccountList=[Account]()
@@ -119,7 +141,7 @@ final class CompanyContact{
         return mergedAccountList
     }
     
-    private func decodeContactsData()->Result<Any>{
+    private func decodeContactsData()->Result<[Account]>{
         
         let result = JsonHandler.readJsonFile(fileName: "contact")
         switch(result) {
@@ -128,17 +150,17 @@ final class CompanyContact{
                 let jsonDecoder = JSONDecoder()
                 do{
                     let contactsList=try jsonDecoder.decode([Account].self, from: data as! Data)
-                    let result=Result.success(contactsList as Any)
+                    let result=Result.success(contactsList)
                     return result
                 }
                 catch let error{
                     print (error)
-                    let result=Result<Any>.failure(error)
+                    let result=Result<[Account]>.failure(error)
                     return result
                 }
             case .failure(let error):
                 print (error)
-                let result=Result<Any>.failure(error)
+                let result=Result<[Account]>.failure(error)
                 return result
         }
     }
