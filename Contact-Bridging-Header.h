7993981729bccafeb3f2f42b9d56fd48eaae6f29
saeedeh <saeedeh@tiseno.com>
2018-07-01 22:15:42 +0800

    add objective C  header  file

---
+++ Contact-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Contact-Bridging-Header.h
+//  Contact
+//
+//  Created by Saeedeh on 01/07/2018.
+//  Copyright © 2018 tiseno. All rights reserved.
+//
+
+#ifndef Contact_Bridging_Header_h
+#define Contact_Bridging_Header_h
+
+#import "TOCropViewController.h"
+#import "SWRevealViewController.h"
+#import "UIImageView+WebCache.h"
+
+#endif /* Contact_Bridging_Header_h */
