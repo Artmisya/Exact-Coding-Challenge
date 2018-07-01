8ffe27cb47ba75a540039b9c9663c080b0501bee
saeedeh <saeedeh@tiseno.com>
2018-07-01 15:35:41 +0800

    adding search

--- Contact/Model/Account/Account.swift
+++ Contact/Model/Account/Account.swift
@@ -48,7 +48,6 @@ class Account:Decodable {
         case phone = "Phone"
         case pictureThumbnailUrl = "PictureThumbnailUrl"
     }
-
     required init(from decoder: Decoder) throws {
         
         let container = try decoder.container(keyedBy: CodingKeys.self)
@@ -117,7 +116,6 @@ class Account:Decodable {
                 pictureThumbnailUrl.append(Value(data: pictureThumbnailUrlData))
         }
     }
-
     init(accountId:String) {
         
         self.accountId=accountId
@@ -132,125 +130,154 @@ class Account:Decodable {
             
             self.businessEmail.append(account.businessEmail[0])
             
-            // set selected to false , let user decide which value is correct
-            account.businessEmail[0].selected=false
-            self.mismatched=true
+            //check if there is more than one value for this key set mismatch to true and  selected to false , let user decide which value is correct
+            if (self.businessEmail.count>1){
+                account.businessEmail[0].selected=false
+                self.mismatched=true
+            }
         }
         if account.businessPhone.count > 0 , self.businessPhone.contains(where:{$0.data == account.businessPhone[0].data})==false{
             
             self.businessPhone.append(account.businessPhone[0])
             
-             // set selected to false , let user decide which value is correct
-            account.businessPhone[0].selected=false
-            self.mismatched=true
+            //check if there is more than one value for this key set mismatch to true and  selected to false , let user decide which value is correct
+            if (self.businessPhone.count>1){
+                account.businessPhone[0].selected=false
+                self.mismatched=true
+            }
         }
         if account.businessPhone.count > 0 ,self.businessMobile.contains(where:{$0.data == account.businessMobile[0].data})==false{
             
             self.businessMobile.append(account.businessMobile[0])
             
-             // set selected to false , let user decide which value is correct
-            account.businessMobile[0].selected=false
-            self.mismatched=true
+            //check if there is more than one value for this key set mismatch to true and  selected to false , let user decide which value is correct
+            if (self.businessPhone.count>1){
+                account.businessPhone[0].selected=false
+                self.mismatched=true
+            }
         }
         if account.email.count > 0 ,self.email.contains(where:{$0.data == account.email[0].data})==false{
             
             self.email.append(account.email[0])
             
-             // set selected to false , let user decide which value is correct
-            account.email[0].selected=false
-            self.mismatched=true
+            //check if there is more than one value for this key set mismatch to true and  selected to false , let user decide which value is correct
+            if (self.email.count>1){
+                account.email[0].selected=false
+                self.mismatched=true
+            }
         }
         if account.firstName.count > 0 ,self.firstName.contains(where:{$0.data == account.firstName[0].data})==false{
             
             self.firstName.append(account.firstName[0])
             
-             // set selected to false , let user decide which value is correct
-            account.firstName[0].selected=false
-            self.mismatched=true
+            //check if there is more than one value for this key set mismatch to true and  selected to false , let user decide which value is correct
+            if (self.firstName.count>1){
+                account.firstName[0].selected=false
+                self.mismatched=true
+            }
         }
         if account.fullName.count > 0 ,self.fullName.contains(where:{$0.data == account.fullName[0].data})==false{
             
             self.fullName.append(account.fullName[0])
             
-             // set selected to false , let user decide which value is correct
-            account.fullName[0].selected=false
-            self.mismatched=true
+            //check if there is more than one value for this key set mismatch to true and  selected to false , let user decide which value is correct
+            if (self.fullName.count>1){
+                account.fullName[0].selected=false
+                self.mismatched=true
+            }
         }
         if account.gender.count > 0 ,self.gender.contains(where:{$0.data == account.gender[0].data})==false{
             
             self.gender.append(account.gender[0])
             
-             // set selected to false , let user decide which value is correct
-            account.gender[0].selected=false
-            self.mismatched=true
+            //check if there is more than one value for this key set mismatch to true and  selected to false , let user decide which value is correct
+            if (self.gender.count>1){
+                account.gender[0].selected=false
+                self.mismatched=true
+            }
         }
         if account.id.count > 0 ,self.id.contains(where:{$0.data == account.id[0].data})==false{
             
             self.id.append(account.id[0])
             
-             // set selected to false , let user decide which value is correct
-            account.id[0].selected=false
-            self.mismatched=true
+            //check if there is more than one value for this key set mismatch to true and  selected to false , let user decide which value is correct
+            if (self.id.count>1){
+                account.id[0].selected=false
+                self.mismatched=true
+            }
         }
         if account.jobTitleDescription.count > 0 , self.jobTitleDescription.contains(where:{$0.data == account.jobTitleDescription[0].data})==false{
             
             self.jobTitleDescription.append(account.jobTitleDescription[0])
             
-             // set selected to false , let user decide which value is correct
-            account.jobTitleDescription[0].selected=false
-            self.mismatched=true
+            //check if there is more than one value for this key set mismatch to true and  selected to false , let user decide which value is correct
+            if (self.jobTitleDescription.count>1){
+                account.jobTitleDescription[0].selected=false
+                self.mismatched=true
+            }
         }
         if account.lastName.count > 0 ,self.lastName.contains(where:{$0.data == account.lastName[0].data})==false{
             
             self.lastName.append(account.lastName[0])
             
-             // set selected to false , let user decide which value is correct
-            account.lastName[0].selected=false
-            self.mismatched=true
+            //check if there is more than one value for this key set mismatch to true and  selected to false , let user decide which value is correct
+            if (self.lastName.count>1){
+                account.lastName[0].selected=false
+                self.mismatched=true
+            }
         }
         if account.middleName.count > 0 ,self.middleName.contains(where:{$0.data == account.middleName[0].data})==false{
             
             self.middleName.append(account.middleName[0])
             
-             // set selected to false , let user decide which value is correct
-            account.middleName[0].selected=false
-            self.mismatched=true
+            //check if there is more than one value for this key set mismatch to true and  selected to false , let user decide which value is correct
+            if (self.middleName.count>1){
+                account.middleName[0].selected=false
+                self.mismatched=true
+            }
         }
         if account.mobile.count > 0 ,self.mobile.contains(where:{$0.data == account.mobile[0].data})==false{
             
-            self.mobile.append(account.mobile[0])
-            
-             // set selected to false , let user decide which value is correct
-            account.mobile[0].selected=false
-            self.mismatched=true
+            //check if there is more than one value for this key set mismatch to true and  selected to false , let user decide which value is correct
+            if (self.mobile.count>1){
+                account.mobile[0].selected=false
+                self.mismatched=true
+            }
         }
         if account.notes.count > 0 ,self.notes.contains(where:{$0.data == account.notes[0].data})==false{
             
             self.notes.append(account.notes[0])
             
-             // set selected to false , let user decide which value is correct
-            account.notes[0].selected=false
-            self.mismatched=true
+            //check if there is more than one value for this key set mismatch to true and  selected to false , let user decide which value is correct
+            if (self.notes.count>1){
+                account.notes[0].selected=false
+                self.mismatched=true
+            }
         }
         if account.phone.count > 0 ,self.phone.contains(where:{$0.data == account.phone[0].data})==false{
             
             self.phone.append(account.phone[0])
             
-             // set selected to false , let user decide which value is correct
-            account.phone[0].selected=false
-            self.mismatched=true
+            //check if there is more than one value for this key set mismatch to true and  selected to false , let user decide which value is correct
+            if (self.phone.count>1){
+                account.phone[0].selected=false
+                self.mismatched=true
+            }
         }
         
         if account.pictureThumbnailUrl.count > 0 ,self.pictureThumbnailUrl.contains(where:{$0.data == account.pictureThumbnailUrl[0].data})==false{
             
             self.pictureThumbnailUrl.append(account.pictureThumbnailUrl[0])
             
-             // set selected to false , let user decide which value is correct
-            account.pictureThumbnailUrl[0].selected=false
-            self.mismatched=true
+             //check if there is more than one value for this key set mismatch to true and  selected to false , let user decide which value is correct
+            if (self.pictureThumbnailUrl.count>1){
+               
+                account.pictureThumbnailUrl[0].selected=false
+                self.mismatched=true
+                
+            }
         }
     }
-    
     func save() -> Result<Bool> {
         
         if (self.isIncomplete()){
@@ -295,6 +322,17 @@ class Account:Decodable {
         return false
     }
     
+    func search (searchKeyword:String)->Bool{
+        
+        if (accountId.lowercased().contains((searchKeyword.lowercased()))){
+            
+            return true
+        }
+        
+        return false
+        
+    }
+    
  // MARK:- private functions
     private func isArrayEmpty(_ inputArray:[Value])->Bool{
         
@@ -302,7 +340,7 @@ class Account:Decodable {
             // has one  value with selected==true
             return false
         }
-        // either the array is empty or has no value with selected==true
+        // either the array is empty or has no value object with selected==true
         return true
     }
     
@@ -311,7 +349,6 @@ class Account:Decodable {
         return inputArray.filter({ $0.selected == false })
     }
 }
-
 
 // MARK:- Expose Account private functions for unit test
 #if DEBUG
@@ -324,7 +361,5 @@ extension Account {
     public func exposePrivateRemoveUnselectedValues(_ inputArray:[Value])-> [Value] {
         return self.removeUnselectedValues(inputArray)
     }
-    
-    
 }
 #endif
