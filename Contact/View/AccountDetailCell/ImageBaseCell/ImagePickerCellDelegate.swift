6433952bf485d3f8d4505fd559040b0d82bfd25a
saeedeh <saeedeh@tiseno.com>
2018-07-02 21:13:46 +0800

    version 1.0

---
+++ Contact/View/AccountDetailCell/ImageBaseCell/ImagePickerCellDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  ImagePickerCellDelegate.swift
+//  Contact
+//
+//  Created by Saeedeh on 02/07/2018.
+//  Copyright © 2018 tiseno. All rights reserved.
+//
+
+import Foundation
+
+
+protocol ImagePickerDelegate {
+    
+    func pickImage(cell: ImageCell)
+}
