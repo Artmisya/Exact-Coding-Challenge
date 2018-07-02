af9c99c3d71df3a4ac8d1f731081b75a1df3462c
saeedeh <saeedeh@tiseno.com>
2018-07-03 00:45:13 +0800

    added emailFormatError

--- Contact/Constants/Constants.swift
+++ Contact/Constants/Constants.swift
@@ -14,15 +14,12 @@ struct Constants {
     struct ApiUrl{
         static let getAccounts:String="https://firebasestorage.googleapis.com/v0/b/contacts-8d05b.appspot.com/o/contacts.json?alt=media&token=431c2754-b3f9-4485-8c5d-0365d5f8f0e5"
     }
-
     struct ErrorMessage{
-        
         static let badUrl:String="Bad Url"
         static let serverError:String="Server Error"
         static let apiError:String="Data received from Api is not in a correct format."
-        static let accountCompulsoryFields:String="Please make sure all compulsory fields are filled in."
+        static let emailFormatError:String="Email format is incorrect."
         static let unknownError="Whoops, something went wrong!"
- 
     }
     struct DomainError{
         static let server:String="ServerError"
@@ -32,30 +29,25 @@ struct Constants {
         static let api:String="apiError"
     }
     
-    
-    struct Account {
-        
-        struct ColorIndicator{
-            
-            static let mismatch:UIColor = UIColor(red: 255.0/255, green: 206.0/255, blue: 91.0/255, alpha: 1.0)
-            static let incomplete:UIColor = UIColor(red: 255.0/255, green: 38.0/255, blue: 0.0/255, alpha: 1.0)
-            static let selectedValue:UIColor = UIColor(red: 143/255.0, green: 188.0/255.0, blue: 143.0/255.0, alpha: 1.0)
-            static let unselectedValue:UIColor = UIColor.white
-            static let compulsoryKey:UIColor = UIColor.red
-        }
-        
+    struct DataRecord {
         struct Message {
-            
-            static let saveAccountSuccess="Saved Successfully!"
+            static let deleteAccountFail="This account doesnot exsit"
             static let deleteAccountSuccess="Removed Successfully!"
         }
-        
+    }
+    struct Account {
+        struct ColorIndicator{
+            static let mismatch:UIColor = UIColor(red: 255.0/255, green: 206.0/255, blue: 91.0/255, alpha: 1.0)
+            static let incomplete:UIColor = UIColor(red: 255.0/255, green: 38.0/255, blue: 0.0/255, alpha: 1.0)
+            static let compulsoryKey:UIColor = UIColor.red
+        }
+        struct Message {
+            static let saveAccountSuccess="Saved Successfully!"
+            static let accountCompulsoryFields:String="Please make sure all compulsory fields are filled in."
+        }
         struct Keys{
-            
             static let KeyNames = ["businessEmail","businessPhone","businessMobile","email","firstName","middleName","lastName","fullName","gender","id","jobTitleDescription","mobile","phone","notes","pictureThumbnailUrl"]
-            
             static let ImageType="pictureThumbnailUrl"
         }
     }
-    
 }
