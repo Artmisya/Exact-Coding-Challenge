00daa82b9f8ecddcdbbd6e29261396653c0c9c22
saeedeh <saeedeh@tiseno.com>
2018-07-03 17:25:22 +0800

    added some comments

--- Contact/Model/Account/Value.swift
+++ Contact/Model/Account/Value.swift
@@ -8,16 +8,28 @@
 
 import Foundation
 
-
+/* isUserInput indicates an userinput value, in case a key doesnot have any value
+ code generate an userinput value for that key so later user can fill it
+ */
 class Value:Codable{
     
-    let data:String
+    var data:String
     var selected:Bool
+   
+    var isUserInput:Bool
+    // in case user select an image
+    var imageData:Data?
     
     init(data:String) {
-        
         self.data=data
         selected=true
+        isUserInput=false
     }
-    
 }
+// in case user select an image
+//class ImageValue:Value{
+//
+//    var imageData:Data?
+//
+//
+//}
